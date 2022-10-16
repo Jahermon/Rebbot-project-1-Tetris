3c7cc7596d1a0f32775c7bf36e1a180ff4f000ca
Jahermon <jaher.front@gmail.com>
2022-10-16 12:32:08 +0100

    Establecido el canvas

--- js/run.js
+++ js/run.js
@@ -1,1 +1,17 @@
-console.log("test")
+var canvas = document.getElementById('canvas');
+
+//especificamos que es un entorno 2d y eso no da metodos para pintar o dibujar elementos
+//https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/getContext
+var contexto = canvas.getContext('2d')
+var fps = 50;
+var anchoCanvas = 400;
+var altoCanvas = 640;
+var anchoTablero =10;
+var altoTablero = 16;
+
+//establecemos el ancho y alto del canvas, lo asigamos por variables por si tenemos que modificar en un futuro
+canvas.width = anchoCanvas;
+canvas.height = altoCanvas;
+
+
+
