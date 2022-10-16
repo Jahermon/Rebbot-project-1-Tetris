66ab6297f513e1101599f37ca105ba11e985f58e
Jahermon <jaher.front@gmail.com>
2022-10-16 21:02:01 +0100

    Tras toda la tarde viendo metodos de canvas mas o menos tenemos una idea de como abordar la logica del proyecto

--- js/run.js
+++ js/run.js
@@ -3,33 +3,258 @@ var canvas = document.getElementById('canvas');
 //especificamos que es un entorno 2d y eso no da metodos para pintar o dibujar elementos
 //https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/getContext
 var contexto = canvas.getContext('2d')
-var fps = 50;
+var anchoTablero = 10;
+var altoTablero = 20;
+var altoFicha = 40;
+var anchoFicha = 40;
 var anchoCanvas = 400;
 var altoCanvas = 640;
-var anchoTablero = 10;
-var altoTablero = 16;
+var margenSuperior = 4;
 
-var Classpieza =  function(){
-    this.x =0;
-    this.y =0;
-    console.log('pieza creada')
 
-    this.rotar = function(){
-        console.log('rotando')
-    }
+// este es el tablero donde el juego se ejecuta un array bidimensional de //12*17  10*16 donde todo lo distinto a 0 será colisión.
+
+
+var tablero = [
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]];
+
+//cada una de las fichas, son 7 fichas con 4 angulos distintos representadas en un array de 4 dimensiones
+//
+var fichaGrafico = [
+  [
+    // [0] cuadrados
+    [
+      [0, 0, 0, 0],
+      [0, 1, 1, 0],
+      [0, 1, 1, 0],
+      [0, 0, 0, 0]
+    ],
+    [
+      [0, 0, 0, 0],
+      [0, 1, 1, 0],
+      [0, 1, 1, 0],
+      [0, 0, 0, 0]
+    ],
+    [
+      [0, 0, 0, 0],
+      [0, 1, 1, 0],
+      [0, 1, 1, 0],
+      [0, 0, 0, 0]
+    ],
+    [
+      [0, 0, 0, 0],
+      [0, 1, 1, 0],
+      [0, 1, 1, 0],
+      [0, 0, 0, 0]
+    ]],
     
-    this.abajo = function(){
-        console.log('moviendo abajo')
-    }
-    
-    this.izquierda = function(){
-        console.log('moviendo izquierda')
-    }
-    
-    this.derecha = function(){
-        console.log('moviendo derecha')
-    }
-    
+  [ // [1] rectangulo v1
+    [
+    [0, 0, 0, 0],
+    [2, 2, 2, 2],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0]
+  ],
+  [
+    [0, 0, 2, 0],
+    [0, 0, 2, 0],
+    [0, 0, 2, 0],
+    [0, 0, 2, 0]
+  ],
+  [
+    [0, 0, 0, 0],
+    [2, 2, 2, 2],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0]
+  ],
+  [
+    [0, 0, 0, 0],
+    [2, 2, 2, 2],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0]
+  ]],
+  [//[2] zeta v1
+    [
+      [0, 0, 0, 0],
+      [0, 0, 3, 3],
+      [0, 3, 3, 0],
+      [0, 0, 0, 0]
+    ],
+    [
+      [0, 0, 3, 0],
+      [0, 0, 3, 3],
+      [0, 0, 0, 3],
+      [0, 0, 0, 0]
+    ],
+    [
+      [0, 0, 0, 0],
+      [0, 0, 3, 3],
+      [0, 3, 3, 0],
+      [0, 0, 0, 0]
+    ],
+    [
+      [0, 0, 3, 0],
+      [0, 0, 3, 3],
+      [0, 0, 0, 3],
+      [0, 0, 0, 0]
+    ]
+  ],
+  [ // [3] zeta v2
+    [
+      [0, 0, 0, 0],
+      [0, 4, 4, 0],
+      [0, 0, 4, 4],
+      [0, 0, 0, 0]
+    ],
+    [
+      [0, 0, 0, 4],
+      [0, 0, 4, 4],
+      [0, 0, 4, 0],
+      [0, 0, 0, 0]
+    ],
+    [
+      [0, 0, 0, 0],
+      [0, 4, 4, 0],
+      [0, 0, 4, 4],
+      [0, 0, 0, 0]
+    ],
+    [
+      [0, 0, 0, 4],
+      [0, 0, 4, 4],
+      [0, 0, 4, 0],
+      [0, 0, 0, 0]
+    ]
+  ],
+  [ // [4] L vv1
+    [
+      [0, 0, 0, 0],
+      [0, 5, 5, 5],
+      [0, 5, 0, 0],
+      [0, 0, 0, 0]
+    ],
+    [
+      [0, 0, 5, 0],
+      [0, 0, 5, 0],
+      [0, 0, 5, 5],
+      [0, 0, 0, 0]
+    ],
+    [
+      [0, 0, 0, 5],
+      [0, 5, 5, 5],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0]
+    ],
+    [
+      [0, 5, 5, 0],
+      [0, 0, 5, 0],
+      [0, 0, 5, 0],
+      [0, 0, 0, 0]
+    ]
+  ],
+  [ //[5] l v2
+    [
+      [0, 0, 0, 0],
+      [0, 6, 6, 6],
+      [0, 0, 0, 6],
+      [0, 0, 0, 0]
+    ],
+    [
+      [0, 0, 6, 6],
+      [0, 0, 6, 0],
+      [0, 0, 6, 0],
+      [0, 0, 0, 0]
+    ],
+    [
+      [0, 6, 0, 0],
+      [0, 6, 6, 6],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0]
+    ],
+    [
+      [0, 0, 6, 0],
+      [0, 0, 6, 0],
+      [0, 6, 6, 0],
+      [0, 0, 0, 0]
+    ]
+  ],
+  [ //[6] triangulo
+    [
+      [0, 0, 0, 0],
+      [0, 7, 7, 7],
+      [0, 0, 7, 0],
+      [0, 0, 0, 0]
+    ],
+    [
+      [0, 0, 7, 0],
+      [0, 0, 7, 7],
+      [0, 0, 7, 0],
+      [0, 0, 0, 0]
+    ],
+    [
+      [0, 0, 7, 0],
+      [0, 7, 7, 7],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0]
+    ],
+    [
+      [0, 0, 7, 0],
+      [0, 7, 7, 0],
+      [0, 0, 7, 0],
+      [0, 0, 0, 0]
+    ]
+  ]
+];
+
+var rojo = '#da0000';
+var verde = '#00e510';
+var azul = '#0046f7';
+var amarillo = '#d2ff00';
+var naranja = '#ff8a00';
+var rosa = '#ff52e0';
+var azulClaro = '#52fff5';
+
+
+var Classpieza = function () {
+  this.x = 0;
+  this.y = 0;
+  console.log('pieza creada')
+
+  this.rotar = function () {
+    console.log('rotando')
+  }
+
+  this.abajo = function () {
+    console.log('moviendo abajo')
+  }
+
+  this.izquierda = function () {
+    console.log('moviendo izquierda')
+  }
+
+  this.derecha = function () {
+    console.log('moviendo derecha')
+  }
+
 }
 
 //establecemos el ancho y alto del canvas, lo asigamos por variables por si tenemos que modificar en un futuro
@@ -38,37 +263,37 @@ canvas.height = altoCanvas;
 
 pieza = new Classpieza();
 
-document.addEventListener('keydown',function(tecla){
-    if(tecla.keyCode === 38){
-      pieza.rotar()
-    }
-    if(tecla.keyCode === 37){
-      pieza.izquierda()
-    }
-    if(tecla.keyCode === 40){
-      pieza.abajo()
-    }
-    if(tecla.keyCode === 39){
-      pieza.derecha()
-    }
-  })
-  
+document.addEventListener('keydown', function (tecla) {
+  if (tecla.keyCode === 38) {
+    pieza.rotar()
+  }
+  if (tecla.keyCode === 37) {
+    pieza.izquierda()
+  }
+  if (tecla.keyCode === 40) {
+    pieza.abajo()
+  }
+  if (tecla.keyCode === 39) {
+    pieza.derecha()
+  }
+})
+
 
 
 setInterval(function () {
-    principal();
+  principal();
 
-    //el bucle se ejecuta 50 veces por segundo 
+  //el bucle se ejecuta 50 veces por segundo 
 }, 1000 / fps);
 
 //borra el contenido del canvas seteando el tamaño, así tenemos generamos la animacion dentro del interval y tenemos control total
 function borrarCanvas() {
-    canvas.width = anchoCanvas;
-    canvas.height = altoCanvas;
+  canvas.width = anchoCanvas;
+  canvas.height = altoCanvas;
 }
 
 //funcion principal del juego
 function principal() {
-    borrarCanvas()
-    console.log("test")
+  borrarCanvas()
+  console.log("test")
 }
