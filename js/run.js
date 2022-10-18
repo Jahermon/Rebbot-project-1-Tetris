f3034a4be65319434adf5d7f25d05fb92098652c
Jahermon <jaher.front@gmail.com>
2022-10-18 11:10:54 +0100

    EL proyecto ha sido refactorizado para una mejor implementacion del juego

--- js/run.js
+++ js/run.js
@@ -1,174 +1,147 @@
-var velocidadJuego = 5
-var filasTabla = 26
-var columnasTabla = 16
-
-//colores
-var rojo = '#da0000';
-var verde = '#00e510';
-var azul = '#0046f7';
-var amarillo = '#d2ff00';
-var naranja = '#ff8a00';
-var rosa = '#ff52e0';
-var azulClaro = '#52fff5';
-
-var numeroAleatorio = Math.floor(Math.random() *7)
-
-function crearTabla() {
-  for (let i = 0; i < filasTabla; i++) {
-    let filaActual = document.getElementById('tablero').insertRow(i)
-    filaActual.classList.add(`row${i}`)
-    for (let j = 0; j < columnasTabla; j++) {
-      let celda = filaActual.insertCell(j)
-      //celda.innerHTML = `10`
-      celda.classList.add(`col${j}`)
-    }
-  }
+const  COLORES = {
+  rojo : '#da0000',
+  verde : '#00e510',
+  azul : '#0046f7',
+  amarillo : '#d2ff00',
+  naranja : '#ff8a00',
+  rosa : '#ff52e0',
+  azulClaro : '#52fff5'
 }
 
-var piezas = [
+const TIPOS_PIEZAS = [
   {
-    pieza: "O",
-    numero: 0,
-    estructura: [{ x: 1, y: 1 }, { x: 1, y: 2 }, { x: 2, y: 1 }, { x: 2, y: 2 }]
+    tipo: "O",
+    pos: [{ x: 7, y: 0 }, { x: 8, y: 0 }, { x:7, y: 1 }, { x: 8, y: 1 }]
   },
   {
-    pieza: "I",
-    numero: 1,
-    estructura: [{ x: 1, y: 1 }, { x: 1, y: 2 }, { x: 1, y: 3 }, { x: 1, y: 4 }]
+    tipo: "I",
+    pos: [{ x: 1, y: 1 }, { x: 1, y: 2 }, { x: 1, y: 3 }, { x: 1, y: 4 }]
   },
   {
-    pieza: "Z",
-    numero: 2,
-    estructura: [{ x: 1, y: 1 }, { x: 2, y: 1 }, { x: 2, y: 2 }, { x: 3, y: 2 }]
+    tipo: "Z",
+    pos: [{ x: 1, y: 1 }, { x: 2, y: 1 }, { x: 2, y: 2 }, { x: 3, y: 2 }]
   },
   {
-    pieza: "S",
-    numero: 3,
-    estructura: [{ x: 3, y: 1 }, { x: 2, y: 1 }, { x: 2, y: 2 }, { x: 1, y: 2 }]
+    tipo: "S",
+    pos: [{ x: 3, y: 1 }, { x: 2, y: 1 }, { x: 2, y: 2 }, { x: 1, y: 2 }]
   },
   {
-    pieza: "T",
-    numero: 4,
-    estructura: [{ x: 1, y: 1 }, { x: 2, y: 2 }, { x: 3, y: 1 }, { x: 2, y: 1 }]
+    tipo: "T",
+    pos: [{ x: 1, y: 1 }, { x: 2, y: 2 }, { x: 3, y: 1 }, { x: 2, y: 1 }]
   },
   {
-    pieza: "L",
-    numero: 5,
-    estructura: [{ x: 2, y: 1 }, { x: 2, y: 2 }, { x: 2, y: 3 }, { x: 3, y: 3 }]
+    tipo: "L",
+    pos: [{ x: 2, y: 1 }, { x: 2, y: 2 }, { x: 2, y: 3 }, { x: 3, y: 3 }]
   },
   {
-    pieza: "J",
-    numero: 6,
-    estructura: [{ x: 2, y: 1 }, { x: 2, y: 2 }, { x: 2, y: 3 }, { x: 1, y: 3 }]
+    tipo: "J",
+    pos: [{ x: 2, y: 1 }, { x: 2, y: 2 }, { x: 2, y: 3 }, { x: 1, y: 3 }]
   },
 ]
 
+function Pieza() {
+  // this.tipo = JSON.parse(JSON.stringify(TIPOS_PIEZAS[Math.floor(Math.random() *7)]));
+  this.tipo = JSON.parse(JSON.stringify(TIPOS_PIEZAS[0])).tipo;
+  this.pos = JSON.parse(JSON.stringify(TIPOS_PIEZAS[0])).pos;
+}
 
-let ultimaPosicion = []
 
-var Classpieza = function () {
-  
-  
+Pieza.prototype.update = function () {
+  this.pos.forEach(pos => {
+    pos.y++
+  })
+}
 
-  this.rotar = function () {
-    console.log("arriba")
-  }
-  this.abajo = function (numero) {
+Pieza.prototype.derecha = function () {
+  this.pos.forEach(pos => {
+    pos.x++
+  })
+}
 
-    piezas.forEach(e => {
-      if (e.numero === numero) {
+Pieza.prototype.izquierda = function () {
+  this.pos.forEach(pos => {
+    pos.x--
+  })
+}
 
-       let estructuraActual = e.estructura.map( e => e )
-       
-       console.log(estructuraActual)
-        estructuraActual.forEach(e => {
-          var pieza = document.querySelector(`.row${e.y++} .col${e.x}`)
-          pieza.classList.add('pieza')
-          console.log(piezas[numeroAleatorio].estructura)
-           })
+
+function Game() {
+  this.velocidadJuego = 1
+  this.filasTabla = 26
+  this.columnasTabla = 16
+  this.timerId = null;
+  this.pieces = []
+
+  this.crearTabla = function() {
+    for (let i = 0; i < this.filasTabla; i++) {
+      let filaActual = document.getElementById('tablero').insertRow(i)
+      filaActual.classList.add(`row${i}`)
+      for (let j = 0; j < this.columnasTabla; j++) {
+        let celda = filaActual.insertCell(j)
+        celda.classList.add(`col${j}`)
       }
-    })
+    }
   }
 
-  this.izquierda = function (numero) {
-    piezas.forEach(e => {
-      if (e.numero === numero) {
-        let estructuraActual = e.estructura.map(e=>e)
-        estructuraActual.forEach(e => {
-          var pieza = document.querySelector(`.row${e.y} .col${e.x--}`)
-          pieza.classList.add('pieza')
-          console.log(piezas[numeroAleatorio].estructura)
-           })
-      }
-    })
-    
-  }
-
-  this.derecha = function (numero) {
-    piezas.forEach(e => {
-      if (e.numero === numero) {
-        let estructuraActual = e.estructura.map(e=>e)
-        estructuraActual.forEach(e => {
-          var pieza = document.querySelector(`.row${e.y} .col${e.x++}`)
-          pieza.classList.add('pieza')
-          console.log(piezas[numeroAleatorio].estructura)
-           })
-      }
-    })
-  }
-
-  this.updatePiece = function () {
-    var pieza = document.querySelectorAll('.pieza')
-    pieza.forEach(function (elem) {
+  this.limpiaTabla = function() {
+    document.querySelectorAll('.pieza').forEach(function (elem) {
       elem.classList.remove('pieza')
     })
   }
 
-  this.pintarPiezas = function (numero) {
-    piezas.forEach(e => {
-      if (e.numero === numero) {
-        let estructuraActual = e.estructura.map(e=>e)
-        estructuraActual.forEach(e => {
-          
-          var pieza = document.querySelector(`.row${e.y++} .col${e.x}`)
-          pieza.classList.add('pieza')
-       })
-      }
+  this.pintaPiezas = function() {
+    this.pieces.forEach(pieza => {
+      pieza.pos.forEach(pos => {
+        if (pos.y < this.filasTabla) {
+          var casilla = document.querySelector(`.row${pos.y} .col${pos.x}`)
+          casilla.classList.add('pieza')
+           
+        }
+      })
     })
   }
 
+  this.movePiezas = function() {
+    this.pieces.forEach(pieza => {
+      pieza.update()
+    })
+  }
+
+  this.movePiece = function(dir) {
+   const piezaActual = this.pieces[0]
+   switch (dir){
+    case 'ArrowLeft':
+      piezaActual.izquierda();
+      break;
+    case 'ArrowRight':
+      piezaActual.derecha();
+      break;   
+    }
+  }
+
+  this.start = function() {
+    this.crearTabla()
+    this.pieces.push(new Pieza())
+
+    this.timerId = setInterval(() => {
+      this.updateGame();
+    }, 1000 / this.velocidadJuego);
+  }
+
+  this.updateGame = function() {
+    this.limpiaTabla()
+    this.pintaPiezas()
+    this.movePiezas()
+  }
 }
-crearTabla()
-pieza = new Classpieza();
-pieza.pintarPiezas()
+
+const game = new Game()
+game.start()
 
 document.addEventListener('keydown', function (tecla) {
-  if (tecla.keyCode === 38) {
-    pieza.rotar()
-  }
-  if (tecla.keyCode === 37) {
-    pieza.izquierda(numeroAleatorio)
-  }
-  if (tecla.keyCode === 40) {
-    pieza.abajo(numeroAleatorio)
-  }
-  if (tecla.keyCode === 39) {
-    pieza.derecha(numeroAleatorio)
+  if ([ 'ArrowLeft','ArrowDown','ArrowRight'].includes(tecla.code)) {
+    game.movePiece(tecla.code)
   }
 })
-
-setInterval(function () {
-  principal();
-
-  //el bucle se ejecuta 50 veces por segundo 
-}, 1000 / velocidadJuego);
-
-
-
-//funcion principal del juego
-function principal() {
-  pieza.updatePiece()
-  pieza.pintarPiezas(numeroAleatorio)
-}
 
 
