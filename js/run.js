0a77eda7ed794c921bedd472ac7f30c71c023b9f
Jahermon <jaher.front@gmail.com>
2022-10-19 18:45:18 +0100

    se borran las filas de manera independiente

--- js/run.js
+++ js/run.js
@@ -88,12 +88,14 @@ Pieza.prototype.izquierda = function () {
   })
 }
 
+
 function Game() {
   this.velocidadJuego = 6
   this.filasTabla = 26
   this.columnasTabla = 16
   this.timerId = null;
   this.pieces = []
+  
 
   this.crearTabla = function () {
     for (let i = 0; i < this.filasTabla; i++) {
@@ -106,27 +108,29 @@ function Game() {
     }
   }
 
+
+  this.borrarFilaCompleta
+   = function (fila) {
+    contador = 0
+    fila.forEach(e => {
+      if (e.classList.contains('tetromino')) {
+        contador++
+        console.log(contador)
+      }
+    })
+    if (contador === 16) {
+      fila.forEach(e => {
+        e.classList.remove('tetromino')
+      })
+      contador = 0
+    }
+  }
+
   this.limpiaTabla = function () {
     document.querySelectorAll('.pieza').forEach(function (elem) {
       elem.classList.remove('pieza')
     })
   }
-
-  /* this.limpiaUltimaFila = function () {
-     var fila = document.querySelectorAll("#tablero tr:last-child td");
-      var contador = 0
-     fila.forEach(e => {
-       if (e.classList.contains('tetromino')) {
-         contador++
-       }
-     })
-     if (contador === 16) {
-       fila.forEach(e => {
-         e.classList.remove('tetromino')
-       })
-       contador = 0
-     }
-   }*/
 
   //cambia la clase de tetro a nada
   this.moverFila = function (fila, numFila) {
@@ -154,49 +158,76 @@ function Game() {
     }
   }
 
+
+  this.monitorizarFilas = function () {
+
+    var row1 = document.querySelectorAll(`.row0 td`)
+    var row2 = document.querySelectorAll(`.row1 td`)
+    var row3 = document.querySelectorAll(`.row2 td`)
+    var row4 = document.querySelectorAll(`.row3 td`)
+    var row5 = document.querySelectorAll(`.row4 td`)
+    var row6 = document.querySelectorAll(`.row5 td`)
+    var row7 = document.querySelectorAll(`.row6 td`)
+    var row8 = document.querySelectorAll(`.row7 td`)
+    var row9 = document.querySelectorAll(`.row8 td`)
+    var row10 = document.querySelectorAll(`.row9 td`)
+    var row11 = document.querySelectorAll(`.row10 td`)
+    var row12 = document.querySelectorAll(`.row11 td`)
+    var row13 = document.querySelectorAll(`.row12 td`)
+    var row14 = document.querySelectorAll(`.row13 td`)
+    var row15 = document.querySelectorAll(`.row14 td`)
+    var row16 = document.querySelectorAll(`.row15 td`)
+    var row17 = document.querySelectorAll(`.row16 td`)
+    var row18 = document.querySelectorAll(`.row17 td`)
+    var row19 = document.querySelectorAll(`.row18 td`)
+    var row20 = document.querySelectorAll(`.row19 td`)
+    var row21 = document.querySelectorAll(`.row20 td`)
+    var row22 = document.querySelectorAll(`.row22 td`)
+    var row23 = document.querySelectorAll(`.row23 td`)
+    var row24 = document.querySelectorAll(`.row24 td`)
+    var row25 = document.querySelectorAll(`.row25 td`)
+
+    this.borrarFilaCompleta(row1)
+    this.borrarFilaCompleta(row2)
+    this.borrarFilaCompleta(row3)
+    this.borrarFilaCompleta(row4)
+    this.borrarFilaCompleta(row5)
+    this.borrarFilaCompleta(row6)
+    this.borrarFilaCompleta(row7)
+    this.borrarFilaCompleta(row8)
+    this.borrarFilaCompleta(row9)
+    this.borrarFilaCompleta(row10)
+    this.borrarFilaCompleta(row11)
+    this.borrarFilaCompleta(row12)
+    this.borrarFilaCompleta(row13)
+    this.borrarFilaCompleta(row14)
+    this.borrarFilaCompleta(row15)
+    this.borrarFilaCompleta(row16)
+    this.borrarFilaCompleta(row17)
+    this.borrarFilaCompleta(row18)
+    this.borrarFilaCompleta(row19)
+    this.borrarFilaCompleta(row20)
+    this.borrarFilaCompleta(row21)
+    this.borrarFilaCompleta(row22)
+    this.borrarFilaCompleta(row23)
+    this.borrarFilaCompleta(row24)
+    this.borrarFilaCompleta(row25)
+  }
+
   //this.limpiaFila
   this.limpiaFila = function () {
     for (i = 25; i >= 0; i--) {
-      for(j = 0; j < 16; j++){
-      var cols = document.querySelector(`.row${i}`).querySelectorAll("td");
-      console.log(cols)
-      if (this.checkFilaCompleta(i)) {
-        cols.forEach(e => {
-          e.classList.remove('tetromino')
-        })
+      for (j = 0; j < 16; j++) {
+        var cols = document.querySelector(`.row${i}`).querySelectorAll("td");
+        console.log(cols)
+        if (this.checkFilaCompleta(i)) {
+          cols.forEach(e => {
+            e.classList.remove('tetromino')
+          })
+        }
+        //this.moverFila()
       }
-      //this.moverFila()
     }
-    }
-  }
-  //  aplicar la limpieza en todas las filas
-  this.DesplazarAbajoLosTetrominos = function () {
-    /*var row1 = document.querySelectorAll(`#tablero  tr:nth-child(1) td`)
-    var row2 = document.querySelectorAll(`#tablero  tr:nth-child(2) td`)
-    var row3 = document.querySelectorAll(`#tablero  tr:nth-child(3) td`)
-    var row4 = document.querySelectorAll(`#tablero  tr:nth-child(4) td`)
-    var row5 = document.querySelectorAll(`#tablero  tr:nth-child(5) td`)
-    var row6 = document.querySelectorAll(`#tablero  tr:nth-child(6) td`)
-    var row7 = document.querySelectorAll(`#tablero  tr:nth-child(7) td`)
-    var row8 = document.querySelectorAll(`#tablero  tr:nth-child(8) td`)
-    var row9 = document.querySelectorAll(`#tablero  tr:nth-child(9) td`)
-    var row10 = document.querySelectorAll(`#tablero  tr:nth-child(10) td`)
-    var row11 = document.querySelectorAll(`#tablero  tr:nth-child(11) td`)
-    var row12 = document.querySelectorAll(`#tablero  tr:nth-child(12) td`)
-    var row13 = document.querySelectorAll(`#tablero  tr:nth-child(13) td`)
-    var row14 = document.querySelectorAll(`#tablero  tr:nth-child(14) td`)
-    var row15 = document.querySelectorAll(`#tablero  tr:nth-child(15) td`)
-    var row16 = document.querySelectorAll(`#tablero  tr:nth-child(16) td`)
-    var row17 = document.querySelectorAll(`#tablero  tr:nth-child(17) td`)
-    var row18 = document.querySelectorAll(`#tablero  tr:nth-child(18) td`)
-    var row19 = document.querySelectorAll(`#tablero  tr:nth-child(19) td`)
-    var row20 = document.querySelectorAll(`#tablero  tr:nth-child(20) td`)
-    var row21 = document.querySelectorAll(`#tablero  tr:nth-child(21) td`)
-    var row22 = document.querySelectorAll(`#tablero  tr:nth-child(22) td`)
-    var row23 = document.querySelectorAll(`#tablero  tr:nth-child(23) td`)
-    var row24 = document.querySelectorAll(`#tablero  tr:nth-child(24) td`)
-    var row25 = document.querySelectorAll(`#tablero  tr:nth-child(25) td`)*/
-
   }
 
   this.pintaPiezas = function () {
@@ -242,10 +273,9 @@ function Game() {
         pieza.movimientoAutomatico()
         this.checkTetromino(pieza)
       } else {
-
         this.fijarPiezaTablero()
         //this.checkFilaCompleta(document.querySelector('.row25')) 
-        this.limpiaFila(document.querySelector('.row25'))
+        //this.limpiaFila(document.querySelector('.row25'))
         //this.moverFila(document.querySelector('.row24'), 24)
         //this.limpiaFila(document.querySelector('.row25'))
         //this.moverFila(document.querySelector('.row24'), 24)
@@ -299,8 +329,10 @@ function Game() {
 
   this.updateGame = function () {
     this.limpiaTabla()
+    this.monitorizarFilas()
     this.pintaPiezas()
     this.movePiezas()
+
   }
 }
 const game = new Game()
